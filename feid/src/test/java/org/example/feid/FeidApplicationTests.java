c9548acb7180687bab49247e9cb44380e1054ed2
AndreSSS1022 <casuareza@academia.usbbog.edu.co>
2025-03-13 00:58:07 -0500

    Add files via upload

---
+++ feid/src/test/java/org/example/feid/FeidApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.feid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
